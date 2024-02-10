14f383d0bd96c05cb8c0993a58ee99deb654dda3
JeffKoMS <jeffko@microsoft.com>
2024-02-10 06:23:35 -0800

    Beginning

---
+++ Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace EasyPolicheck;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}

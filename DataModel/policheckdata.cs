c2f741b21603e1801a71f1f67a2542201624a22a
JeffKoMS <jeffko@microsoft.com>
2024-02-26 09:02:48 -0800

    goodbye policheck data class

--- DataModel/policheckdata.cs
+++
@@ -1,8 +0,0 @@
-namespace EasyPolicheck.Data;
-using System.ComponentModel.DataAnnotations;
-
-public class PolicheckLocation
-{
-    [Required]
-    public string? PCLocation { get; set; } = string.Empty;
-}
